1ea275b725146a00488753b96a01ca0e4021b5ba
David Ackroyd <davidackroydmusic@gmail.com>
2020-01-15 14:53:55 +0000

    added stylesheet and copying of resources

--- scripts/build-site.sh
+++ scripts/build-site.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 
 OUT_FILE="../www/index.html"
+OUT_DIR="../www"
 HEADER_TEMPLATE="../templates/header.html"
 FACT_START_TEMPLATE="../templates/fact-start.html"
 FACT_END_TEMPLATE="../templates/fact-end.html"
 FOOTER_TEMPLATE="../templates/footer.html"
 FACTS_DIR="../facts/*"
-
+RESOURCES_DIR="../resources/*"
 
 rm $OUT_FILE 
 cat $HEADER_TEMPLATE >> $OUT_FILE
+
+yes | cp -rf $RESOURCES_DIR $OUT_DIR
 
 for file in $FACTS_DIR
 do 
