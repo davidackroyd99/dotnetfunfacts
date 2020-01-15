d251a211899fc5ce0b8f1112d6f9ac006c3e82bd
David Ackroyd <davidackroydmusic@gmail.com>
2020-01-15 16:05:26 +0000

    each fact on own webpage

--- scripts/build-site.sh
+++ scripts/build-site.sh
@@ -11,7 +11,8 @@ FACTS_DIR="../facts/*"
 RESOURCES_DIR="../resources/*"
 OUT_DIR="../www"
 
-rm $OUT_FILE 
+rm -r $OUT_DIR
+mkdir $OUT_DIR
 
 cat $HEADER_TEMPLATE >> $OUT_FILE
 
@@ -19,9 +20,19 @@ yes | cp -rf $RESOURCES_DIR $OUT_DIR
 
 for file in $FACTS_DIR
 do 
+	fact_name=$(head -1 $file | cut -c4-)
+	fact_file=$(echo $fact_name | tr " " _ | tr . _)
+	fact_file="${fact_file}.html"
+	
 	cat $FACT_START_TEMPLATE >> $OUT_FILE
-	markdown $file >> $OUT_FILE
+	echo "<a href='${fact_file}'>${fact_name}</a>" >> $OUT_FILE
 	cat $FACT_END_TEMPLATE >> $OUT_FILE
+
+	fact_file_path="${OUT_DIR}/${fact_file}"
+	cat $HEADER_TEMPLATE >> $fact_file_path
+	markdown $file >> $fact_file_path
+	cat $FOOTER_TEMPLATE >> $fact_file_path
+
 done
 
 cat $FOOTER_TEMPLATE >> $OUT_FILE
