2d751a3e6cb349e0aae52c51bce6f53d434987fd
David Ackroyd <davidackroydmusic@gmail.com>
2020-01-18 00:09:10 +0000

    changed paths on site build script so that it can be executed from the root

--- scripts/build-site.sh
+++ scripts/build-site.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
-OUT_FILE="../www/index.html"
+OUT_FILE="www/index.html"
 
-HEADER_TEMPLATE="../templates/header.html"
-FACT_START_TEMPLATE="../templates/fact-start.html"
-FACT_END_TEMPLATE="../templates/fact-end.html"
-FOOTER_TEMPLATE="../templates/footer.html"
+HEADER_TEMPLATE="templates/header.html"
+FACT_START_TEMPLATE="templates/fact-start.html"
+FACT_END_TEMPLATE="templates/fact-end.html"
+FOOTER_TEMPLATE="templates/footer.html"
 
-FACTS_DIR="../facts/*"
-RESOURCES_DIR="../resources/*"
-OUT_DIR="../www"
+FACTS_DIR="facts/*"
+RESOURCES_DIR="resources/*"
+OUT_DIR="www/"
 
 rm -r $OUT_DIR
 mkdir $OUT_DIR
 
 cat $HEADER_TEMPLATE >> $OUT_FILE
 
-echo "factsArray = [" >> "${OUT_DIR}/data.js"
+echo "factsArray = [" >> "${OUT_DIR}data.js"
 
 yes | cp -rf $RESOURCES_DIR $OUT_DIR
 
@@ -26,20 +26,20 @@ do
 	fact_file=$(echo $fact_name | tr " " _ | tr . _) # replace spaces and dots with underscores
 	fact_file="${fact_file}.html" # add .html
 	
-	echo "\"${fact_file}\"," >>  "${OUT_DIR}/data.js" # write this to the js file that contains all the data for 
+	echo "\"${fact_file}\"," >>  "${OUT_DIR}data.js" # write this to the js file that contains all the data for 
 														# the random fact functionality and so on
 	cat $FACT_START_TEMPLATE >> $OUT_FILE
 	echo "<a href='${fact_file}'>${fact_name}</a>" >> $OUT_FILE # write the title of each fact out to the index.html page
 	cat $FACT_END_TEMPLATE >> $OUT_FILE
 
-	fact_file_path="${OUT_DIR}/${fact_file}" # finally actually make the fact page
+	fact_file_path="${OUT_DIR}${fact_file}" # finally actually make the fact page
 	cat $HEADER_TEMPLATE >> $fact_file_path
 	markdown $file >> $fact_file_path
 	cat $FOOTER_TEMPLATE >> $fact_file_path
 
 done
 
-echo "]" >> "${OUT_DIR}/data.js"
+echo "]" >> "${OUT_DIR}data.js"
 
 
 cat $FOOTER_TEMPLATE >> $OUT_FILE
