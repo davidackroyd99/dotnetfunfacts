9c1af1d540653a0c402b67bdc342f1592abb51da
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-18 00:25:47 +0000

    Deploy davidackroyd99/dotnetfunfacts to github.com/davidackroyd99/dotnetfunfacts.git:gh-pages

---
+++ data.js
@@ -0,0 +1,10 @@
+factsArray = [
+"Importing_and_Exporting_CSV_Data_From_SQLite3.html",
+"SQL_String_Comparisons_Are_Not_Case_Sensitive.html",
+"Checking_For_Empty_Strings_With_String_IsNullOrWhitespace.html",
+"Null_Confusion.html",
+"Common_Cause_of_an_InvalidOperationException.html",
+"Order_of_Elements_In_A_Project_File.html",
+"Easy_Date_Printing.html",
+"The_Difference_Between_Readonly_and_Const.html",
+]
