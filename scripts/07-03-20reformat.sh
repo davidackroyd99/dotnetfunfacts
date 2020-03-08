3676b32bd46e35f76dd4be9fe282e6974ed294a0
David Ackroyd <davidackroydmusic@gmail.com>
2020-03-08 00:22:16 +0000

    automatically set title tag

--- scripts/07-03-20reformat.sh
+++ scripts/07-03-20reformat.sh
@@ -2,11 +2,12 @@ FACTS_DIR="../facts/*"
 
 for file in $FACTS_DIR
 do
+	fact_name=$(head -1 $file | cut -c4-)
 	contents=$(cat $file)
 	rm $file
 	echo "!meta
 
-title:
+title:${fact_name}
 contributors:David Ackroyd
 created:2020-03-08
 modified:2020-03-08
