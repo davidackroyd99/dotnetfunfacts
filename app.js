d080373fffbb3f3563472b2bc566586690a86d84
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-15 03:40:19 +0000

    Deploy davidackroyd99/dotnetfunfacts to github.com/davidackroyd99/dotnetfunfacts.git:gh-pages

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var app = new Vue({
     el: '#app',
     data: {
         facts: [],
+        tags: [],
+        selectedTags: [],
         currentFact: window.location.hash.substr(1)
     },
     created: function() {
@@ -9,6 +11,13 @@ var app = new Vue({
             .then(response => response.json())
             .then(data => {
                 this.facts = data.facts;
+
+                for(let i = 0; i < this.facts.length; i++) {
+                    for(let j = 0; j < this.facts[i].metadata.tags.length; j++) {
+                        if(!this.tags.includes(this.facts[i].metadata.tags[j]))
+                            this.tags.push(this.facts[i].metadata.tags[j]);
+                    }
+                }
             });
     },
     methods: {
@@ -21,6 +30,42 @@ var app = new Vue({
             }
 
             window.location.hash = url;
+        },
+        selectTag: function(tag) {
+            if(!this.selectedTags.includes(tag)) {
+                this.selectedTags.push(tag);
+            }
+            else {
+                this.selectedTags.splice(this.selectedTags.indexOf(tag), 1);
+            }
+        },
+        clearTags: function() {
+            this.selectedTags = [];
+        },
+        getFacts: function() {
+            if(this.selectedTags.length == 0) {
+                return this.facts;
+            }
+            else {
+                let taggedFacts = [];
+
+                for(let i = 0; i < this.facts.length; i++) {
+                    let addToCollection = true;
+
+                    for(let j = 0; j < this.selectedTags.length; j++) {
+                        if(!this.facts[i].metadata.tags.includes(this.selectedTags[j])) {
+                            addToCollection = false;
+                            break;
+                        }
+                    }
+
+                    if(addToCollection) {
+                        taggedFacts.push(this.facts[i]);
+                    }
+                }
+
+                return taggedFacts;
+            }
         }
     }
 });
