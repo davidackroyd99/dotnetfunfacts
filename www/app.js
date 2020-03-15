5dcf0788971aaa21364332337e45f68af2f95cbb
David Ackroyd <davidackroydmusic@gmail.com>
2020-03-15 02:59:27 +0000

    collect and render tags

--- www/app.js
+++ www/app.js
@@ -2,6 +2,7 @@ var app = new Vue({
     el: '#app',
     data: {
         facts: [],
+        tags: [],
         currentFact: window.location.hash.substr(1)
     },
     created: function() {
@@ -9,6 +10,13 @@ var app = new Vue({
             .then(response => response.json())
             .then(data => {
                 this.facts = data.facts;
+
+                for(let i = 0; i < this.facts.length; i++) {
+                    for(let j = 0; j < this.facts[i].metadata.tags.length; j++) {
+                        if(!this.tags.includes(this.facts[i].metadata.tags[j]))
+                            this.tags.push(this.facts[i].metadata.tags[j]);
+                    }
+                }
             });
     },
     methods: {
